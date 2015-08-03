6974d3038ecb180dbdd9693cd50de12708d2afbc
cash <cash@cashwu.com>
2015-08-03 18:59:25 +0800

    StoreAppTestController Add Nunit

---
+++ StoreAppTestController.Nunit.Tests/Products2ControllerTest.cs
@@ -0,0 +1,125 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using StoreAppTestController.Controllers;
+using StoreAppTestController.Models;
+using System.Data.Entity;
+using System.Net;
+using System.Web.Http.Results;
+
+namespace StoreAppTestController.Nunit.Tests
+{
+    [TestFixture]
+    public class Products2ControllerTest
+    {
+        [Test]
+        public void GetReturnsProductWithSameId()
+        {
+             // arrange
+            var item = this.GetDemoProduct();
+
+            var mockDbSet = Substitute.For<DbSet<Product>>();
+            var mockDbContext = Substitute.For<StoreAppTestControllerContext>();
+            mockDbContext.Products = mockDbSet;
+            mockDbContext.Products.Find(item.Id).Returns(item);
+
+            var controller = new Products2Controller(mockDbContext);
+
+            // act
+            var result = controller.Get(1);
+
+            // assert  
+            result.Should().NotBeNull();
+            result.As<OkNegotiatedContentResult<Product>>().Content.Id.Should().Be(1);
+        }
+
+        [Test]
+        public void GetReturnsNotFound()
+        {
+            // arrange
+            var item = this.GetDemoProduct();
+
+            var mockDbSet = Substitute.For<DbSet<Product>>();
+            var mockDbContext = Substitute.For<StoreAppTestControllerContext>();
+            mockDbContext.Products = mockDbSet;
+            mockDbContext.Products.Find(item.Id).Returns(item);
+
+            var controller = new Products2Controller(mockDbContext);
+
+            // act
+            var result = controller.Get(10);
+
+            // assert
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Test]
+        public void DeleteReturnsOk()
+        {
+            // arrange
+            var item = this.GetDemoProduct();
+
+            var mockDbSet = Substitute.For<DbSet<Product>>();
+            var mockDbContext = Substitute.For<StoreAppTestControllerContext>();
+            mockDbContext.Products = mockDbSet;
+            mockDbContext.Products.Find(item.Id).Returns(item);
+            mockDbContext.Products.Remove(item).Returns(item);
+
+            var controller = new Products2Controller(mockDbContext);
+
+            // act
+            var result = controller.Delete(1);
+
+            // assert
+            result.Should().BeOfType<OkResult>();
+        }
+
+        [Test]
+        public void PostMethodSetsLocationHeader()
+        {
+            // arrange
+            var item = this.GetDemoProduct();
+
+            var mockDbSet = Substitute.For<DbSet<Product>>();
+            var mockDbContext = Substitute.For<StoreAppTestControllerContext>();
+            mockDbContext.Products = mockDbSet;
+            mockDbContext.Products.Add(item).Returns(item);
+
+            var controller = new Products2Controller(mockDbContext);
+
+            // act
+            var result = controller.Post(item);
+
+            // assert
+            result.Should().NotBeNull().And.BeOfType<CreatedAtRouteNegotiatedContentResult<Product>>();
+            result.As<CreatedAtRouteNegotiatedContentResult<Product>>().RouteName.Should().Be("DefaultApi");
+            result.As<CreatedAtRouteNegotiatedContentResult<Product>>().RouteValues["id"].Should().Be(1);
+        }
+
+        [Test]
+        public void PutReturnsContentResult()
+        {
+            // arrange
+            var item = this.GetDemoProduct();
+
+            var mockDbSet = Substitute.For<DbSet<Product>>();
+            var mockDbContext = Substitute.For<StoreAppTestControllerContext>();
+            mockDbContext.Products = mockDbSet;
+
+            var controller = new Products2Controller(mockDbContext);
+
+            // act
+            var result = controller.Put(item);
+
+            // assert
+            result.Should().NotBeNull();
+            result.As<NegotiatedContentResult<Product>>().StatusCode.Should().Be(HttpStatusCode.Accepted);
+            result.As<NegotiatedContentResult<Product>>().Content.Id.Should().Be(1);
+        }
+
+        private Product GetDemoProduct()
+        {
+            return new Product() { Id = 1, Name = "Demo name", Price = 5 };
+        }
+    }
+}
---
+++ StoreAppTestController.Nunit.Tests/ProductsControllerTest.cs
@@ -0,0 +1,107 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using StoreAppTestController.Controllers;
+using StoreAppTestController.Models;
+using System;
+using System.Data.Entity;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace StoreAppTestController.Nunit.Tests
+{
+    [TestFixture]
+    public class ProductsControllerTest
+    {
+        [Test]
+        public void GetReturnsProduct()
+        {
+            // arrange
+            var item = this.GetDemoProduct();
+            var mockDbSet = Substitute.For<DbSet<Product>>();
+            var mockDbContext = Substitute.For<StoreAppTestControllerContext>();
+            mockDbContext.Products = mockDbSet;
+            mockDbContext.Products.Find(item.Id).Returns(item);
+
+            var controller = new ProductsController(mockDbContext);
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            // act
+            var result = controller.Get(1);
+
+            // assert
+            Product product;
+            result.TryGetContentValue<Product>(out product).Should().BeTrue();
+            product.Id.Should().Be(1);
+        }
+
+        [Test]
+        public void PostSetsLocationHeader()
+        {
+            // arrange
+            var item = this.GetDemoProduct();
+
+            var mockDbSet = Substitute.For<DbSet<Product>>();
+            var mockDbContext = Substitute.For<StoreAppTestControllerContext>();
+            mockDbContext.Products = mockDbSet;
+            mockDbContext.Products.Add(item).Returns(item);
+
+            var controller = new ProductsController(mockDbContext);
+            controller.Request = new HttpRequestMessage
+            {
+                RequestUri = new Uri("http://localhost/api/products")
+            };
+
+            controller.Configuration = new HttpConfiguration();
+            controller.Configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional});
+            
+            controller.RequestContext.RouteData = new HttpRouteData(
+                route: new HttpRoute(),
+                values: new HttpRouteValueDictionary() { { "controller", "products" } });
+
+            // act
+            var result = controller.Post(item);
+
+            // assert
+            result.Headers.Location.AbsoluteUri.Should().Be("http://localhost/api/products/1");
+        }
+
+        [Test]
+        public void PostSetsLocationHeader_MockVersion()
+        {
+            // arrange
+            var item = this.GetDemoProduct();
+
+            var mockDbSet = Substitute.For<DbSet<Product>>();
+            var mockDbContext = Substitute.For<StoreAppTestControllerContext>();
+            mockDbContext.Products = mockDbSet;
+            mockDbContext.Products.Add(item).Returns(item);
+
+            var controller = new ProductsController(mockDbContext);
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            string locationUrl = "http://location/";
+
+            var mockUrlHelper = Substitute.For<UrlHelper>();
+            mockUrlHelper.Link(Arg.Any<string>(), Arg.Any<object>()).Returns(locationUrl);
+            controller.Url = mockUrlHelper;
+
+            // act
+            var result = controller.Post(item);
+
+            // assert
+            result.Headers.Location.AbsoluteUri.Should().Be(locationUrl);
+        }
+
+        private Product GetDemoProduct()
+        {
+            return new Product() { Id = 1, Name = "Demo name", Price = 5 };
+        }
+    }
+}
